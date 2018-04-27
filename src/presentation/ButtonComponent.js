02604b1694a2633df3bde2b114bd91592b28539e
bartekma89 <bartekma89@gmail.com>
2018-04-27 19:06:46 +0200

    added button component and formatChange method

--- src/presentation/ButtonComponent.js
+++ src/presentation/ButtonComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({ sortBy, sortKey, title }) => {
-	return <button onClick={sortBy.bind(this, sortKey)}>{title}</button>;
+	return <a onClick={sortBy.bind(this, sortKey)}>{title}</a>;
 };
 
 export default Button;
