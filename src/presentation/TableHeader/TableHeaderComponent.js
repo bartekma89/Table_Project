c8a58d5b36a76fd996bbac332c4146ac9361223e
bartekma89 <bartekma89@gmail.com>
2018-04-29 15:24:19 +0200

    fixed error in table style

--- src/presentation/TableHeader/TableHeaderComponent.js
+++ src/presentation/TableHeader/TableHeaderComponent.js
@@ -4,44 +4,44 @@ import './TableHeader.css';
 
 const TableHeader = ({ sortBy }) => {
 	return (
-		<thead>
-			<tr className="row">
-				<th className="record-header id">
+		<thead className="tableHeader">
+			<tr className="header-row">
+				<th className="header-record id">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="id"
 						title="iD"
 					/>
 				</th>
-				<th className="record-header firstName">
+				<th className="header-record firstName">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="firstName"
 						title="First Name"
 					/>
 				</th>
-				<th className="record-header lastName">
+				<th className="header-record lastName">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="lastName"
 						title="Last Name"
 					/>
 				</th>
-				<th className="record-header dateOfBirth">
+				<th className="header-record dateOfBirth">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="dateOfBirth"
 						title="Birth Date"
 					/>
 				</th>
-				<th className="record-header company">
+				<th className="header-record company">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="company"
 						title="Company"
 					/>
 				</th>
-				<th className="record-header note">
+				<th className="header-record note">
 					<Button
 						sortBy={sortBy.bind(this)}
 						sortKey="note"
